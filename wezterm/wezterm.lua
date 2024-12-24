0311c0315f9fc40b4c34b343f9932795c28f51e2
Manny Barreto <ma.barre98@gmail.com>
2024-12-24 08:19:03 -0500

    Minor font changes for wezterm.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16.0
 
 config.color_scheme = 'rose-pine'
